9439d352bddcf8b22afe4fd9b79b908cbfee8be8
Ben Thompson <ben.t11@hotmail.com>
2024-06-23 22:38:06 -0700

    mirror changes from main

--- api/server.js
+++ api/server.js
@@ -34,7 +34,7 @@ getDataPoints();
 
 mongoose.connect("mongodb://127.0.0.1:27017/")
 	.then(() => console.log("Connected to DB"))
-	.then(() => setInterval(getDataPoints, 1000 * 10)) // only want to start interval when we can properly store the data
+	//.then(() => setInterval(getDataPoints, 1000 * 10)) // only want to start interval when we can properly store the data
 	.catch((err) => console.error(err));
 
 const Sample = require("./models/Sample");
@@ -42,6 +42,38 @@ const Sample = require("./models/Sample");
 router.get("/data", async (req, res) => {
 	const datapoints = await Sample.find().sort({"time": 1});
 	res.json(datapoints);
+});
+
+router.get("/data/average", async (req, res) => {
+	var averages = [];
+	var from_date = await Sample.findOne().sort("+time")
+		.then((date) => {
+			return new Date(date.time).getTime();
+		});
+
+	const to_date = await Sample.findOne().sort("-time")
+		.then((date) => {
+			return new Date(date.time).getTime();
+		});
+
+	while (from_date < to_date) {
+		const outer_range = from_date + 1000*60*60;
+		var datapoints = await Sample.aggregate([
+			{ $match : { 
+				time: { $gte: new Date(from_date), $lt: new Date(outer_range)},
+				food: { $gte: 16000000, $lt: 40000000 } } },
+			{ $group: { _id: null, food: { $avg: "$food" } } }
+		]).then((data) => {
+			return data[0];
+		});
+		if (datapoints !== undefined) {
+			datapoints.time = new Date(outer_range);
+			averages.push(datapoints);
+		}
+		from_date = outer_range;
+	}
+
+	res.json(averages);
 });
 
 router.post("/data", async (req, res) => {
