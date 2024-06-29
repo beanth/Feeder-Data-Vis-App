988d1c66961b796dc56a52cdde36a7aa510a7bc0
Ben Thompson <ben.t11@hotmail.com>
2024-06-29 12:46:09 -0700

    fix datapoints being saved out of order in mongo

--- api/server.js
+++ api/server.js
@@ -21,20 +21,19 @@ async function getDataPoints() {
 				time: new Date(response[i][0])
 			});
 
-			datapoint.save();
+			await datapoint.save();
 		}
 	}
 	catch(e) {
 		console.error("ERROR:", e);
 	}
-	//const datapoint
 }
 
 getDataPoints();
 
 mongoose.connect("mongodb://127.0.0.1:27017/")
 	.then(() => console.log("Connected to DB"))
-	//.then(() => setInterval(getDataPoints, 1000 * 10)) // only want to start interval when we can properly store the data
+	.then(() => setInterval(getDataPoints, 1000 * 10)) // only want to start interval when we can properly store the data
 	.catch((err) => console.error(err));
 
 const Sample = require("./models/Sample");
@@ -43,11 +42,13 @@ router.get("/data", async (req, res) => {
 	const to_date = await Sample.findOne().sort("-time")
 		.then((date) => {
 			return new Date(date.time).getTime();
-		});
-	const from_date = to_date - 1000*60*60*12;
+		})
+		.catch((err) => console.error(err));
+	const from_date = to_date - 1000*60*60*24*2;
 
 	const datapoints = await Sample.find({"time": {"$gte": from_date, "$lte": to_date}})
-		.sort("+time");
+		.sort("+time")
+		.catch((err) => console.error(err));
 	res.json(datapoints);
 });
 
@@ -56,19 +57,23 @@ router.get("/data/average", async (req, res) => {
 	var from_date = await Sample.findOne().sort("+time")
 		.then((date) => {
 			return new Date(date.time).getTime();
-		});
+		})
+		.catch((err) => console.error(err));
 
 	const to_date = await Sample.findOne().sort("-time")
 		.then((date) => {
 			return new Date(date.time).getTime();
-		});
+		})
+		.catch((err) => console.error(err));
 
 	while (from_date < to_date) {
-		var outer_range = Math.min(from_date + 1000*60*60, to_date);
+		const outer_range = Math.min(from_date + 1000*60*60, to_date);
 		var datapoints = await Sample.aggregate([
 			{ $match : { 
 				time: { $gte: new Date(from_date), $lt: new Date(outer_range)},
-				food: { $gte: 16000000, $lt: 40000000 } } },
+				food: { $gte: 6000000, $lt: 40000000 }
+				//5000000
+			} },
 			{ $group: { _id: null, food: { $avg: "$food" } } }
 		]).then((data) => {
 			return data[0];
