519f77d0802c840b6bd249eeee59b22cf6471dda
Ben Thompson <ben.t11@hotmail.com>
2024-06-29 12:47:46 -0700

    change chart to timescale

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import CamFeed from './CamFeed';
 import { LineChart, Line, Legend, Tooltip, XAxis, YAxis, CartesianGrid, ReferenceLine } from 'recharts';
 
 const API = "http://localhost:3001";
-const MAX_FOOD = 32000000;
-const MIN_FOOD = 8000000;
+const MAX_FOOD = 40000000;
+const MIN_FOOD = 6000000;//8000000;
 
 function App() {
 	const [samples, setSamples] = useState([]);
@@ -12,24 +12,33 @@ function App() {
 	const [foodNumber, setFoodNumber] = useState(13);
 	const [dateValue, setDateValue] = useState("");
 	const [refillAmt, setRefillAmt] = useState(20);
+
+	var minDate, maxDate;
+	var domain = [0, 10];
 	
 	useEffect(() => {
 		GetDataPoints();
-		console.log(samples.length);
 		//setInterval(GetDataPoints, 1000 * 10);
 	}, []);
 
 	async function GetDataPoints() {
 		// TODO:
 		//	OPTIMIZE QUERY TO ONLY FETCH MISSING DATAPOINTS
-		fetch(API + "/data/average")
+		fetch(API + "/data/")
 			.then(response => response.json())
 			.then((data) => {
 				for (var i in data) {
-					data[i].time = new Date(data[i].time).toLocaleString([],
-						{ hour: '2-digit', minute: '2-digit' });
+					data[i].time = new Date(data[i].time);
+					if (maxDate === undefined || data[i].time.getTime() > maxDate.getTime())
+						maxDate = data[i].time;
+
+					if (minDate === undefined || data[i].time.getTime() < minDate.getTime())
+						minDate = data[i].time;
+					//data[i].time = data[i].time.toLocaleString([],
+					//	{ timeZone: 'UTC', hour: 'numeric', minute: 'numeric', hour12: true });
 					data[i].food = ((data[i].food - MIN_FOOD) / MAX_FOOD * 100).toFixed(1);
 				}
+				//domain = [minDate.getTime(), maxDate.getTime()];
 				setSamples(data);
 			})
 			.catch(error => console.error("Error: ", error));
@@ -69,17 +78,19 @@ function App() {
 		})
 			.then(res => res.json())
 			.catch(error => console.error("Error: ", error));
-		if (data !== null)
-			setSamples(samples => samples.filter(sample => sample._id !== data._id));
+		//if (data !== null)
+		//	setSamples(samples => samples.filter(sample => sample._id !== data._id));
 	}
 
 	return (
 		<div className="App" style={{ margin: 10 }}>
 			<h1 style={{ color: "blue" }}>Cat Feeder</h1>
-			<LineChart width={730} height={400} data={samples}
-				margin={{ top: 30, right: 30, left: 30, bottom: 30 }}>
+			<LineChart width={750} height={400} data={samples}
+				margin={{ top: 20, right: 20, bottom: 20 }}>
 				<CartesianGrid strokeDasharray="6 12"/>
-				<XAxis dataKey="time" angle={-25} textAnchor='end' height={55}/>
+				<XAxis dataKey="time" scale='time'  angle={-25}
+					textAnchor='end' height={55} tickFormatter={ value => value.toLocaleString([],
+						{ hour: 'numeric', minute: 'numeric', hour12: true }) }/>
 				<YAxis dataKey="food" tickFormatter={ value => `${value}%` }/>
 				<ReferenceLine y={refillAmt} position={"top"} label={{
 		          position: "bottom",
@@ -97,16 +108,22 @@ function App() {
 				<button type="submit">Submit</button>
 			</form>
 			{/*<div className="samples">
-			{samples.map(samples => (
-				<div key={samples._id} className="data" style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+			{samples.map(samples => {
+				var color = 'red';
+				if (samples.food > 60)
+					color = "white";
+				if (samples.food > 80)
+					color = "darkred";
+				return (
+				<div key={samples._id} className="data" style={{backgroundColor: color, display: "flex", flexDirection: "row", alignItems: "center"}}>
 					<div>
-					{ samples.time }<br/>
+					{ samples.time.toString() }<br/>
 					{ samples.food }
 					&nbsp;{ samples.text }
 					</div>
 					&emsp;<button id={samples._id} onClick={handleDelete}>X</button>
 				</div>
-			))}
+			)})}
 			</div>*/}
 		</div>
 	);
