ec31f05ed478d320a733ba8247e62c1e5fe089c4
Ben Thompson <ben.t11@hotmail.com>
2024-07-13 11:10:36 -0700

    remove TimedImage as camfeed is now vid stream

--- client/src/CamFeed.js
+++ client/src/CamFeed.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { TimedImage } from "react-timed-image";
 
 const IMG_API = "http://67.168.172.32:5010";//"http://pi.local:5010";
 
-function CamFeed() {
-	return (
-		<TimedImage src={IMG_API} interval={1000}/>
-	)
+function CamFeed(props) {
+	return <img src={IMG_API} {...props} alt="Food Bowl CamFeed"/>;
 }
 
 export default CamFeed;
